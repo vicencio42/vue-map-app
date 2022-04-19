3d9977666fa6c7dbe1610008b2b336979ec66b6c
Vicencio42 <vicencio42@gmail.com>
2022-04-18 19:46:48 -0500

    initial and protected API token

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is a map!!</h1>
+    <div id="map"></div>
   </div>
 </template>
+
+<script>
+/* global mapboxgl */
+export default {
+  data: function () {
+    return {};
+  },
+  mounted: function () {
+    mapboxgl.accessToken = process.env.VUE_APP_MY_API_KEY;
+    const map = new mapboxgl.Map({
+      container: "map", // container ID
+      style: "mapbox://styles/mapbox/satellite-v9", // style URL
+      center: [-105.207451, 39.666508], // starting position [lng, lat]
+      zoom: 16, // starting zoom
+    });
+    console.log(map);
+  },
+  methods: {},
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+#map {
+  width: 100%;
+  height: 400px;
+}
+</style>
